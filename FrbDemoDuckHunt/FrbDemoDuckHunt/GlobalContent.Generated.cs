02f7a8f7170942f5c3e58da6cfb2554eda4361c3
Scott Dancer <dancer.scott@gmail.com>
2013-10-09 20:20:10 -0500

    Added animations for Dog.

--- FrbDemoDuckHunt/FrbDemoDuckHunt/GlobalContent.Generated.cs
+++ FrbDemoDuckHunt/FrbDemoDuckHunt/GlobalContent.Generated.cs
@@ -15,13 +15,24 @@ namespace FrbDemoDuckHunt
 	public static partial class GlobalContent
 	{
 		
+		public static Microsoft.Xna.Framework.Graphics.Texture2D DogHunt { get; set; }
 		[System.Obsolete("Use GetFile instead")]
 		public static object GetStaticMember (string memberName)
 		{
+			switch(memberName)
+			{
+				case  "DogHunt":
+					return DogHunt;
+			}
 			return null;
 		}
 		public static object GetFile (string memberName)
 		{
+			switch(memberName)
+			{
+				case  "DogHunt":
+					return DogHunt;
+			}
 			return null;
 		}
 		public static bool IsInitialized { get; private set; }
@@ -30,6 +41,7 @@ namespace FrbDemoDuckHunt
 		public static void Initialize ()
 		{
 			
+			DogHunt = FlatRedBallServices.Load<Microsoft.Xna.Framework.Graphics.Texture2D>(@"content/globalcontent/doghunt.png", ContentManagerName);
 						IsInitialized = true;
 		}
 		public static void Reload (object whatToReload)
